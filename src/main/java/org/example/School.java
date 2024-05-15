41bec60de164b46f388b99e0e70ef5c8f437fb97
Sergejus <Sergejus.balciunas@gmail.com>
2024-05-15 21:54:34 +0300

    patrinti nereikalingi metodai

--- src/main/java/org/example/School.java
+++ src/main/java/org/example/School.java
@@ -142,27 +142,6 @@ public class School {
         }
 
     }
-    public static void sort(String[] array) {
-        boolean swapped;
-        int n = array.length;
-        do {
-            swapped = false;
-            for (int i = 1; i < n; i++) {
-
-                if (array[i - 1].compareTo(array[i]) > 0) {
-                    String temp = array[i - 1];
-                    array[i - 1] = array[i];
-                    array[i] = temp;
-                    swapped = true;
-                }
-            }
-            n--;
-        } while (swapped);
-    }
-
-
-
-
 
 
 }
